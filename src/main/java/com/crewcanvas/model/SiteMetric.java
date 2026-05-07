2eb1c4cf27dfd4902ab9ad23722d7b855e5c1bbf
vnelapatla <158700174+vnelapatla@users.noreply.github.com>
2026-05-07 09:45:41 +0530

    Fix: Switching from javax to jakarta for SiteMetric

--- src/main/java/com/crewcanvas/model/SiteMetric.java
+++ src/main/java/com/crewcanvas/model/SiteMetric.java
@@ -1,8 +1,8 @@
 package com.crewcanvas.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 
 @Entity
